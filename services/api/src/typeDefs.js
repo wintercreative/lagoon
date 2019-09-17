82fe1d143a7527a8eadf69790b055121f605ea42
wintercreative <justin@wintercreative.com>
2019-09-17 16:32:15 -0400

    addBillingToProject resolvers

--- services/api/src/typeDefs.js
+++ services/api/src/typeDefs.js
@@ -942,6 +942,11 @@ const typeDefs = gql`
     groups: [GroupInput!]!
   }
 
+  input BillingGroupInput {
+    name: String!
+    project: ProjectInput!
+  }
+
   type Mutation {
     """
     Add Environment or update if it is already existing
@@ -1038,6 +1043,7 @@ const typeDefs = gql`
     addUserToGroup(input: UserGroupRoleInput!): Group
     removeUserFromGroup(input: UserGroupInput!): Group
     addGroupsToProject(input: ProjectGroupsInput): Project
+    addBillingGroupToProject(input: BillingGroupInput): Project
     removeGroupsFromProject(input: ProjectGroupsInput!): Project
   }
 
